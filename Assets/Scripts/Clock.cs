f42b5e16413a6cf166b7b38124d0e85bcb572fa8
shermenz <dinbaram@gmail.com>
2019-02-20 13:22:12 +0200

    First commit

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public Transform HoursArm, MinutesArm, SecondsArm;
+    public bool IsContinuous;
+
+    private const float DegreesPerHour = 30f;
+    private const float DegreesPerMinutes = 6f;
+    private const float DegreesPerSeconds = 6f;
+
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (IsContinuous)
+            UpdateContinues();
+        else
+            UpdateDiscrete();
+    }
+
+    // Update is called once per frame
+    void UpdateContinues()
+    {
+        var time = DateTime.Now.TimeOfDay;
+        HoursArm.localRotation = Quaternion.Euler(0f, (float)time.TotalHours * DegreesPerHour, 0f);
+        MinutesArm.localRotation = Quaternion.Euler(0f, (float)time.TotalMinutes * DegreesPerMinutes, 0f);
+        SecondsArm.localRotation = Quaternion.Euler(0f, (float)time.TotalSeconds * DegreesPerSeconds, 0f);
+    }
+
+    void UpdateDiscrete()
+    {
+        var time = DateTime.Now;
+        HoursArm.localRotation = Quaternion.Euler(0f, time.Hour * DegreesPerHour, 0f);
+        MinutesArm.localRotation = Quaternion.Euler(0f, time.Minute * DegreesPerMinutes, 0f);
+        SecondsArm.localRotation = Quaternion.Euler(0f, time.Second * DegreesPerSeconds, 0f);
+    }
+}
---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    public Transform PointPrefab;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Vector3 scale = Vector3.one / 5f;
+        Vector3 position = Vector3.zero;
+
+        for (int i = 0; i < 10; i++)
+        {
+            var point = Instantiate(PointPrefab);
+            position.x = (i + 0.5f) / 5f - 1f;
+            position.y = position.x;
+            point.localPosition = position;
+            point.localScale = scale;
+        }
+    }
+}
