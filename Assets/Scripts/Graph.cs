3ff1305ab9737183b6aa897b7f4c4139e7434396
shermenz <dinbaram@gmail.com>
2019-02-21 12:00:04 +0200

    Graph completed

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -5,20 +5,41 @@ using UnityEngine;
 public class Graph : MonoBehaviour
 {
     public Transform PointPrefab;
+    [Range(10, 100)]
+    public int Resolution = 10;
+
+    Transform[] points;
 
     // Start is called before the first frame update
     void Awake()
     {
-        Vector3 scale = Vector3.one / 5f;
+        points = new Transform[Resolution];
+
+        float step = 2f / Resolution;
+        Vector3 scale = Vector3.one * step;
         Vector3 position = Vector3.zero;
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < points.Length; i++)
         {
             var point = Instantiate(PointPrefab);
-            position.x = (i + 0.5f) / 5f - 1f;
-            position.y = position.x;
+            points[i] = point;
+
+            position.x = (i + 0.5f) * step - 1f;
             point.localPosition = position;
             point.localScale = scale;
+
+            point.SetParent(transform, false);
+        }
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            var position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+            point.localPosition = position;
         }
     }
 }
