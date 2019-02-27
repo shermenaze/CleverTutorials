4f5d0746876d8f8da1e76a3a99c36924c6950012
shermenz <dinbaram@gmail.com>
2019-02-27 13:29:21 +0200

    Geometrical shapes part 2

--- Assets/ColoredPoint.shader
+++ Assets/ColoredPoint.shader
@@ -13,7 +13,7 @@
 
         void surf (Input IN, inout SurfaceOutputStandard o)
         {
-			o.Albedo.rg = IN.worldPos.xy * 0.5f + 0.5f;
+			o.Albedo.rgb = IN.worldPos.xyz * 0.5f + 0.5f;
         }
         ENDCG
     }
--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -4,61 +4,61 @@ using UnityEngine;
 
 public class Graph : MonoBehaviour
 {
-    [Range(0, 1)]
-    public int function;
+    public GraphFunctionName function;
     [Range(10, 100)]
     public int Resolution = 10;
     public Transform PointPrefab;
 
+    static GraphFunction[] functions =
+        { SineFunction, MultiSineFunction };
     Transform[] points;
 
     // Start is called before the first frame update
     void Awake()
     {
-        points = new Transform[Resolution];
+        points = new Transform[Resolution * Resolution];
 
         float step = 2f / Resolution;
         Vector3 scale = Vector3.one * step;
         Vector3 position = Vector3.zero;
 
-        for (int i = 0; i < points.Length; i++)
+        for (int i = 0, z = 0; z < Resolution; z++)
         {
-            var point = Instantiate(PointPrefab);
-            points[i] = point;
+            for (int x = 0; x < points.Length; x++, i++)
+            {
+                var point = Instantiate(PointPrefab);
+                points[i] = point;
 
-            position.x = (i + 0.5f) * step - 1f;
-            point.localPosition = position;
-            point.localScale = scale;
+                position.x = (x + 0.5f) * step - 1f;
+                position.z = (z + 0.5f) * step - 1f;
+                point.localPosition = position;
+                point.localScale = scale;
 
-            point.SetParent(transform, false);
+                point.SetParent(transform, false);
+            }
         }
     }
 
     private void Update()
     {
         float t = Time.time;
+        GraphFunction f = functions[(int)function];
+
         for (int i = 0; i < points.Length; i++)
         {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            if (function == 0)
-            {
-                position.y = SineFunction(position.x, t);
-            }
-            else
-            {
-                position.y = MultiSineFunction(position.x, t);
-            }
+            position.y = f(position.x, position.z, t);
             point.localPosition = position;
         }
     }
 
-    static float SineFunction(float x, float t)
+    static float SineFunction(float x, float z, float t)
     {
         return Mathf.Sin(Mathf.PI * (x + t));
     }
 
-    static float MultiSineFunction(float x, float t)
+    static float MultiSineFunction(float x, float z, float t)
     {
         float y = Mathf.Sin(Mathf.PI * (x + t));
         y += Mathf.Sin(2f * Mathf.PI * (x + 2f * t)) / 2f;
