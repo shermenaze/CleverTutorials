d9018b22f9ab60c9a98e42c0e6f3b155c2a667e2
shermenz <dinbaram@gmail.com>
2019-02-27 11:47:41 +0200

    Geometrical shapes

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Graph : MonoBehaviour
 {
-    public Transform PointPrefab;
+    [Range(0, 1)]
+    public int function;
     [Range(10, 100)]
     public int Resolution = 10;
+    public Transform PointPrefab;
 
     Transform[] points;
 
@@ -34,12 +36,33 @@ public class Graph : MonoBehaviour
 
     private void Update()
     {
+        float t = Time.time;
         for (int i = 0; i < points.Length; i++)
         {
             Transform point = points[i];
-            var position = point.localPosition;
-            position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+            Vector3 position = point.localPosition;
+            if (function == 0)
+            {
+                position.y = SineFunction(position.x, t);
+            }
+            else
+            {
+                position.y = MultiSineFunction(position.x, t);
+            }
             point.localPosition = position;
         }
+    }
+
+    static float SineFunction(float x, float t)
+    {
+        return Mathf.Sin(Mathf.PI * (x + t));
+    }
+
+    static float MultiSineFunction(float x, float t)
+    {
+        float y = Mathf.Sin(Mathf.PI * (x + t));
+        y += Mathf.Sin(2f * Mathf.PI * (x + 2f * t)) / 2f;
+        y *= 2f / 3f;
+        return y;
     }
 }
