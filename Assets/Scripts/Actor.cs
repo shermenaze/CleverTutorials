c948a8b402f88a5876146f7a5c06f95549f3a6ce
shermenz <dinbaram@gmail.com>
2019-03-07 13:15:16 +0200

    Command Design Pattern

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Actor : MonoBehaviour
+    {
+        InputHandler _input;
+        Command _inputCommand;
+
+        private void Start()
+        {
+            _input = gameObject.AddComponent<InputHandler>();
+        }
+
+        public void Jump()
+        {
+            Debug.Log("Jump");
+        }
+
+        internal void Fire()
+        {
+            Debug.Log("Fire");
+        }
+
+        private void Update()
+        {
+            _inputCommand = _input.HandleInput();
+
+            if (_inputCommand != null)
+                _inputCommand.Execute(this);
+        }
+    }
+}
